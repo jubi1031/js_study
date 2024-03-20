06370bf880f947a51115cc6b00c3ffc3aa6cc8bf
jubi <joobe1031@gmail.com>
2024-03-20 16:16:18 +0900

    arry

---
+++ 1arry.js
@@ -0,0 +1,17 @@
+'use strict'; //엄격모드
+//객체 {}
+// 배열 []
+
+let arry = [0, 1, 2, 3, 4];
+let arry2 = ['안녕', '반갑습니다', '또만나요'];
+console.log(typeof arry);
+console.log(arry[0]);
+console.log(arry2[arry2.length - 1]);
+
+//배열에 아이틈을 추가하는 방법
+//
+//배열의 앞에 추가
+arry.unshift('t');
+//배열의 뒤에 추가
+arry.push(6);
+console.log(arry, arry.lengthk);
