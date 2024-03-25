a73419f85380134d1bd99b5e9652c041c0227bb8
jubi <joobe1031@gmail.com>
2024-03-25 09:23:43 +0900

    builtin

---
+++ j07/j02builtin.js
@@ -0,0 +1,13 @@
+//
+const num = 123.568;
+const num2 = new Number(123);
+console.log(typeof num2);
+
+console.log(num.toFixed()); //124반올림
+console.log(num.toString());
+console.log(num.toLocaleString('ko-KR'));
+console.log(num.toPrecision(5));
+
+console.log(Math.pow(5, 2));
+console.log(Math.PI);
+console.log(Math.E);
