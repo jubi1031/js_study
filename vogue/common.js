801f9bec03fe60d5b14ac2ed58c25d2a3c6bd54d
jubi <joobe1031@gmail.com>
2024-03-26 10:29:22 +0900

    vogue 완료

--- vogue/common.js
+++ vogue/common.js
@@ -2,8 +2,8 @@ const btnHam = document.querySelector('.ham');
 const $header = document.querySelector('.hd');
 const menu = document.querySelectorAll('.gnb > a');
 const main = document.querySelector('main');
-const section = main.querySelectorAll('.box');
-console.log(section);
+const section = main.querySelectorAll('main>section');
+console.log(section[2]);
 
 // .ham 클릭 시 스스로에게 .off 적용
 // .ham 클릭 시 header에 .on 적용
@@ -15,7 +15,7 @@ btnHam.addEventListener('click', (e) => {
 });
 
 // .gnb>a 클릭하면 header에 .on제거되고, .ham 에 .off  제거
-menu.forEach((menuBtn) => {
+menu.forEach((menuBtn, i) => {
   menuBtn.addEventListener('click', (e) => {
     e.preventDefault();
     console.log('클릭됬다');
